b0cc91de6fd5d864490a1110c558188386670bd4
Ben Galde <bgalde.dev@gmail.com>
2021-03-03 03:45:28 -0800

    Added data cleanup

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -1,4 +1,82 @@
 // from data.js
-var tableData = data;
+//var tableData = data;
+var tableData = [{
+    datetime: "1/1/2010",
+    city: "benton",
+    state: "ar",
+    country: "us",
+    shape: "circle",
+    durationMinutes: "5 mins.",
+    comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+},
+{
+    datetime: "1/1/2010",
+    city: "bonita",
+    state: "ca",
+    country: "us",
+    shape: "light",
+    durationMinutes: "13 minutes",
+    comments: "Three bright red lights witnessed floating stationary over San Diego New Years Day 2010"
+}];
 
-// YOUR CODE HERE!
+// Change a string to title case for city names.
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+// Clean the data so it looks nice
+var cleanTableData = tableData.map(element => {
+    return {
+        datetime: element.datetime.toString().toUpperCase(),
+        city: toTitleCase(element.city.toString()),
+        state: element.state.toString().toUpperCase(),
+        country: element.country.toString().toUpperCase(),
+        shape: toTitleCase(element.shape.toString()),
+        durationMinutes: element.durationMinutes,
+        comments: element.comments
+    };
+});
+
+// Insert Data into HTML
+var tbody = d3.select("tbody");
+
+cleanTableData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#filter-form");
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create the function to run for both events
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Print the value to the console
+  console.log(inputValue);
+
+  // Set the span tag in the h1 element to the text
+  // that was entered in the form
+  // d3.select("h1>span").text(inputValue);
+  // FILTER THE DATA
+}
